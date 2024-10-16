0221a1bc897b92b4dd0e32c27bd9fa8676514f32
gurmeet1011 <156289823+gurmeet1011@users.noreply.github.com>
2024-10-16 15:42:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
-//complete this code
-class Person {}
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this._age = age; // Use _age to avoid conflict with the setter method
+  }
 
-class Student extends Person {}
+  // Setter for age
+  set age(s) {
+    this._age = s;
+  }
 
-class Teacher extends Person {}
+  // Getter for age
+  get age() {
+    return this._age;
+  }
+
+  // Getter for name
+  get name() {
+    return this._name;
+  }
+}
+
+class Student extends Person {
+  study() {
+    console.log(this.name + " is studying");
+  }
+}
+
+class Teacher extends Person {
+  teach() {
+    console.log(this.name + " is teaching");
+  }
+}
 
 // Do not change the code below this line
 window.Person = Person;
